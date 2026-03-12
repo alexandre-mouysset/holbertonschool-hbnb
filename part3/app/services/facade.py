f52235d3c2781f2b0ea2a0cb3ad44443cf7613c0
alexandre-mouysset <12258@holbertonstudents.com>
2026-03-12 14:22:50 +0100

    feat(repository): migrate to SQLAlchemy for data persistence in repositories

--- part3/app/services/facade.py
+++ part3/app/services/facade.py
@@ -2,15 +2,15 @@ from app.models.amenity import Amenity
 from app.models.place import Place
 from app.models.review import Review
 from app.models.user import User
-from app.persistence.repository import InMemoryRepository
+from app.persistence.repository import SQLAlchemyRepository
 
 
 class HBnBFacade:
     def __init__(self):
-        self.user_repo = InMemoryRepository()
-        self.place_repo = InMemoryRepository()
-        self.review_repo = InMemoryRepository()
-        self.amenity_repo = InMemoryRepository()
+        self.user_repo = SQLAlchemyRepository()
+        self.place_repo = SQLAlchemyRepository()
+        self.review_repo = SQLAlchemyRepository()
+        self.amenity_repo = SQLAlchemyRepository()
 
 # ----- user ----------------------------------
 
